329b64971bccf3539e4e02edfdabb0d4d75fb263
AdrianeRuggiero <driketes@gmail.com>
2023-12-05 20:34:05 +0100

    adds fontfamily fixes style gallery

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -2,41 +2,12 @@ import './collapse.scss'
 import arrow from '../../assets/arrow.png';
 import { useState } from 'react';
 
-/* export default function Collapse({title, content}) {
 
-    const [toggle, setToggle] = useState(false);
+function Collapse ({ title, content }) {
+    const [open, setOpen] = useState(false);
 
-    return (
-        <>
-            <div className="collapse" >
-                <h3 className='collapse_title' onClick={() => setToggle(!toggle)} >
-                    {title}
-                    <img 
-                        className={toggle ? 'arrow arrow_up' : 'arrow arrow_down'} 
-                        src={arrow} 
-                        alt="show content" 
-                    />
-                </h3>
-                <div className={toggle ? 'collapse_content' : 'collapse_content_hidden'}>
-                    {Array.isArray(content) ? content.map((item, index) => {
-                        return (
-                            <p key={index}>{item}</p>
-                        )
-                    }) : content
-                    }
-                </div> 
-            </div>
-        </>
-    )
-}*/
-
-import React from 'react';
-
-const Collapse = ({ title, content }) => {
-    const [toggle, setToggle] = useState(false);
-
-    const toggleCollapse = () => {
-        setToggle(!toggle);
+    const changeCollapse = () => {
+        setOpen(!open);
     };
 
     const renderContent = () => {
@@ -48,15 +19,15 @@ const Collapse = ({ title, content }) => {
 
     return (
         <div className="collapse">
-            <h3 className="collapse_title" onClick={toggleCollapse}>
+            <h3 className="collapse_title" onClick={changeCollapse}>
                 {title}
                 <img
-                    className={`arrow ${toggle ? 'arrow_up' : 'arrow_down'}`}
+                    className={`arrow ${open ? 'arrow_up' : 'arrow_down'}`}
                     src={arrow}
-                    alt="show content"
+                    alt="arrow for content"
                 />
             </h3>
-            <div className={toggle ? 'collapse_content' : 'collapse_content_hidden'}>
+            <div className={open ? 'collapse_content' : 'collapse_content_hidden'}>
                 {renderContent()}
             </div>
         </div>
